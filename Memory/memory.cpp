410f79b723386672c720525ed9cd86329425ba4a
Tufo88 <a.armengol2004@gmail.com>
2023-03-16 23:58:37 +0100

    uyuyuy

--- Memory/memory.cpp
+++ Memory/memory.cpp
@@ -1,2 +1,39 @@
 #include "memory.h"
 
+#include <TlHelp32.h>
+#include <iostream>
+
+#define LOG(x) std::cout << "\n\n=====================================================\nPROCESS NAME: " << x.szExeFile << "\n  Process ID = " << x.th32ProcessID << " \n"
+
+//constructor 
+Memory::Memory(const char* processName, bool searchProcessOrWindow) {
+	PROCESSENTRY32 processEntry; //struct en el que se guarda toda la info del proceso
+	processEntry.dwSize = sizeof(PROCESSENTRY32); //no se, es obligatorio hacerlo para que funcione correctamente
+
+	HANDLE hProcessSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); //se guardan/(crea un snapshot de todos) los procesos ejecutandose
+
+
+	if (hProcessSnapshot == INVALID_HANDLE_VALUE) std::cout << "xd \n";
+	
+
+	while (Process32Next(hProcessSnapshot, &processEntry)) {
+		LOG(processEntry); //mostrar en cmd todos los procesos
+		Sleep(10000);
+
+		if (processName == processEntry.szExeFile) { //guardar el puntero al proceso en mi Memory y el id del proceso
+			this->processId = processEntry.th32ProcessID;
+			this->process = OpenProcess(PROCESS_ALL_ACCESS, 0, this->processId);
+
+		}
+
+	}
+	
+
+	if (hProcessSnapshot) CloseHandle(hProcessSnapshot);
+}
+
+//destructor 
+Memory::~Memory() {
+
+	if (this->process) CloseHandle(this->process);
+}
--- Memory/memory.h
+++ Memory/memory.h
@@ -1,32 +1,34 @@
 #ifndef MEMORY__H_
 #define MEMORY__H_
 
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-struct MemoryRead {
-	template <typename S>
-	S value;
-	bool success;
-};
 
 class Memory{
 
 private: 
-	HANDLE process = nullptr; // aka void* process -> used to get the pointer of the process (the handle to process)
+	HANDLE process = nullptr; // aka void* process -> used to get the pointer of the process (the handle to a process)
 	DWORD processId = 0; // aka unsigned long processId
+
 public:
-	Memory(); //constructor
+	Memory(const char* processName, bool searchProcessOrWindow); //constructor
 	~Memory(); //destructor 
 
-	DWORD getProcessId() { return processId; }
-	HANDLE getProcessHandle() { return process;  }
+	DWORD getProcessId() { return this->processId; }
+	HANDLE getProcessHandle() { return this->process;  }
 
 
 	template <typename T>
-	MemoryRead readMemory(uintptr_t address) {
+	T Read(uintptr_t address) {
 		T value;
 		ReadProcessMemory(this->process, (LPCVOID)address, &value, sizeof(T), nullptr);
 		return value;
+	}
+
+	template <typename T>
+	bool Write(uintptr_t address, T value) {
+		return WriteProcessMemory(this->process, (LPCVOID)address, &value, sizeof(T), nullptr);
 	}
 
 
